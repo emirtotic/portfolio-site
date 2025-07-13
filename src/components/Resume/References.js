5fb6ebff5b6497a8a91c77b848d52cb1e4dbed8b
Emir Totic <59924947+emirtotic@users.noreply.github.com>
2025-07-13 15:44:07 +0200

    Update References.js

--- src/components/Resume/References.js
+++ src/components/Resume/References.js
@@ -6,28 +6,28 @@ const References = () => (
     <div className="link-to" id="references" />
     <div className="title">
       <h2>My Skills</h2>
-      <ul>
-        <li>Java - Java EE</li>
-        <li>Spring Framework</li>
-        <li>Spring Boot & MVC</li>
-        <li>Spring Security</li>
-        <li>Spring REST</li>
-        <li>Reactive Spring</li>
-        <li>Microservices</li>
-        <li>Hibernate</li>
-        <li>MySQL Database</li>
-        <li>NoSQL Databases</li>
-        <li>MongoDB</li>
-        <li>PostgreSQL</li>
-        <li>Kafka</li>
-        <li>REST Integrations</li>
-        <li>Git / GitHub</li>
-        <li>Bitbucket</li>
-        <li>Jira / AGILE</li>
-        <li>Maven</li>
-        <li>Docker</li>
-        <li>Eclipse, IntelliJ, NetBeans</li>
-      </ul>
+      <ul style={{ columns: 2, columnGap: '2rem' }}>
+          <li>Java - Java EE</li>
+          <li>Spring Framework</li>
+          <li>Spring Boot & MVC</li>
+          <li>Spring Security</li>
+          <li>Spring REST</li>
+          <li>Reactive Spring</li>
+          <li>Microservices</li>
+          <li>Hibernate</li>
+          <li>MySQL Database</li>
+          <li>NoSQL Databases</li>
+          <li>MongoDB</li>
+          <li>PostgreSQL</li>
+          <li>Kafka</li>
+          <li>REST Integrations</li>
+          <li>Git / GitHub</li>
+          <li>Bitbucket</li>
+          <li>Jira / AGILE</li>
+          <li>Maven</li>
+          <li>Docker</li>
+          <li>Eclipse, IntelliJ, NetBeans</li>
+        </ul>
       <Link to="/contact">
         <h3>For more details, feel free to contact me</h3>
       </Link>
