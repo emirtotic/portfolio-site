b51e0bda50567a50456ea51948a50be49e2aa5c4
emirtotic <emirtotic@gmail.com>
2025-07-15 17:28:38 +0200

    update resume

--- src/components/Resume/References.js
+++ src/components/Resume/References.js
@@ -7,30 +7,30 @@ const References = () => (
     <div className="title">
       <h2>My Skills</h2>
       <ul style={{ columns: 2, columnGap: '2rem' }}>
-          <li>Java - Java EE</li>
-          <li>Spring Framework</li>
-          <li>Spring Boot & MVC</li>
-          <li>Spring Security</li>
-          <li>Spring REST</li>
-          <li>Reactive Spring</li>
-          <li>Microservices</li>
-          <li>Hibernate</li>
-          <li>MySQL Database</li>
-          <li>NoSQL Databases</li>
-          <li>MongoDB</li>
-          <li>PostgreSQL</li>
-          <li>Neo4j</li>
-          <li>Kafka</li>
-          <li>REST Integrations</li>
-          <li>Git / GitHub</li>
-          <li>Bitbucket</li>
-          <li>Jira / AGILE</li>
-          <li>Maven</li>
-          <li>Docker</li>
-          <li>Rust</li>
-          <li>Rust</li>
-          <li>Axum, Tokio, Tonic</li>
-        </ul>
+        <li>Java - Java EE</li>
+        <li>Spring Framework</li>
+        <li>Spring Boot & MVC</li>
+        <li>Spring Security</li>
+        <li>Spring REST</li>
+        <li>Reactive Spring</li>
+        <li>Microservices</li>
+        <li>Hibernate</li>
+        <li>MySQL Database</li>
+        <li>NoSQL Databases</li>
+        <li>MongoDB</li>
+        <li>PostgreSQL</li>
+        <li>Neo4j</li>
+        <li>Kafka</li>
+        <li>REST Integrations</li>
+        <li>Git / GitHub</li>
+        <li>Bitbucket</li>
+        <li>Jira / AGILE</li>
+        <li>Maven</li>
+        <li>Docker</li>
+        <li>Rust</li>
+        <li>Rust</li>
+        <li>Axum, Tokio, Tonic</li>
+      </ul>
       <Link to="/contact">
         <h3>For more details, feel free to contact me</h3>
       </Link>
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -16,9 +16,24 @@ const Main = (props) => (
       defaultTitle="Emir Totić"
       defer={false}
     >
+      {/* Title and Description */}
       {props.title && <title>{props.title}</title>}
       <meta name="description" content={props.description} />
+
+      {/* Additional SEO Meta Tags */}
+      <meta name="author" content="Emir Totic" />
+      <meta name="keywords" content="Java Developer, Backend Developer, Spring, Hibernate, Emir Totić, Portfolio" />
+      <meta property="og:title" content={props.title || 'Emir Totic'} />
+      <meta property="og:description" content={props.description || 'Java Backend Developer Portfolio'} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://emirtotic.github.io/portfolio-site" />
+      <meta property="og:image" content="https://emirtotic.github.io/portfolio-site/path_to_your_image.jpg" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={props.title || 'Emir Totic'} />
+      <meta name="twitter:description" content={props.description || 'Java Backend Developer Portfolio'} />
+      <meta name="twitter:image" content="https://emirtotic.github.io/portfolio-site/path_to_your_image.jpg" />
     </Helmet>
+
     <div id="wrapper">
       <Navigation />
       <div id="main">{props.children}</div>
